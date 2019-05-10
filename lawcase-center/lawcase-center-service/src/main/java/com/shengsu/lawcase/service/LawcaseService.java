1265c0b5685464f33c3040d41aa17c2ea3bb3572
zxh <zzhaguo@hotmail.com>
2019-05-10 11:13:59 +0800

    工程模块拆分为：client、service

---
+++ lawcase-center/lawcase-center-service/src/main/java/com/shengsu/lawcase/service/LawcaseService.java
@@ -0,0 +1,10 @@
+package com.shengsu.lawcase.service;
+
+import com.shengsu.base.service.BaseService;
+import com.shengsu.lawcase.entity.Lawcase;
+
+/**
+ * Created by zxh on 2019/5/8.
+ */
+public interface LawcaseService extends BaseService<Lawcase,String>{
+}
