26bda6deb2cd2ecd8e842127ebaf504419985e62
zxh <zzhaguo@hotmail.com>
2019-05-10 13:18:17 +0800

    包名修正

---
+++ lawcase-center/lawcase-center-service/src/main/java/com/shengsu/lawcase/service/impl/LawcaseServiceImpl.java
@@ -0,0 +1,20 @@
+package com.shengsu.lawcase.service.impl;
+
+import com.shengsu.base.service.impl.BaseServiceImpl;
+import com.shengsu.lawcase.entity.Lawcase;
+import com.shengsu.lawcase.mapper.LawcaseMapper;
+import com.shengsu.lawcase.service.LawcaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by zxh on 2019/5/8.
+ */
+public class LawcaseServiceImpl extends BaseServiceImpl<Lawcase,String> implements LawcaseService{
+    LawcaseMapper lawcaseMapper;
+
+    @Autowired
+    public void setlawcaseMapper(LawcaseMapper lawcaseMapper){
+        this.lawcaseMapper = lawcaseMapper;
+        this.baseMapper = lawcaseMapper;
+    }
+}
