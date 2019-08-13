069073793e71bf264879b40a2b0c1af9015771d2
zyc <xinmima520>
2019-08-13 10:01:23 +0800

    新闻资讯后台管理-Oss工具类

--- lawcase-center/lawcase-center-service/src/main/java/com/shengsu/bench/service/impl/BenchJournalismServiceImpl.java
+++ lawcase-center/lawcase-center-service/src/main/java/com/shengsu/bench/service/impl/BenchJournalismServiceImpl.java
@@ -7,9 +7,9 @@ import com.shengsu.bench.constant.BenchConstant;
 import com.shengsu.bench.entity.BenchJournalism;
 import com.shengsu.bench.mapper.BenchJournalismMapper;
 import com.shengsu.bench.service.BenchJournalismService;
+import com.shengsu.util.OssClientUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -18,11 +18,13 @@ import java.util.List;
  * Created by zyc on 2019/8/9.
  */
 @Service(value = "benchJournalismService")
-public class BenchJournalismServiceImpl  extends BaseServiceImpl<BenchJournalism,String> implements BenchJournalismService {
+public class BenchJournalismServiceImpl extends BaseServiceImpl<BenchJournalism, String> implements BenchJournalismService {
 
     @Autowired
     private BenchJournalismMapper benchJournalismMapper;
 
+    @Autowired
+    OssClientUtil ossClientUtil;
 
     @Override
     public BaseMapper<BenchJournalism, String> getBaseMapper() {
@@ -33,30 +35,38 @@ public class BenchJournalismServiceImpl  extends BaseServiceImpl<BenchJournalism
     /**
      * 功能描述: <br>
      * 〈〉新闻资讯分页查询
+     *
      * @Param: [journalism]
      * @Return: com.shengsu.base.domain.ResultBean
      * @Author: zyc
      * @Date: 2019/8/8 15:00
      */
     @Override
-    public List<BenchJournalism> listByPage(BenchJournalism journalism){
-
+    public List<BenchJournalism> listByPage(BenchJournalism journalism) {
         //分页获取
-        List<BenchJournalism> benchJournalisms = benchJournalismMapper.selectJournalismByPage(journalism);
-        if(benchJournalisms==null||benchJournalisms.isEmpty()){
+        List<BenchJournalism> journalismList = benchJournalismMapper.selectJournalismByPage(journalism);
+        if (journalismList == null || journalismList.isEmpty()) {
             return null;
         }
-        return benchJournalisms;
+
+        for (BenchJournalism benchJournalism :
+                journalismList) {
+            String pictureOssId = benchJournalism.getPictureOssId();
+            journalism.setPictureOssUrl(ossClientUtil.getUrl(BenchConstant.OSS_FILEDIR, pictureOssId));
+        }
+        return journalismList;
     }
+
     /**
      * 功能描述: <br>
      * 〈获取总条数〉
+     *
      * @Param: [benchJournalism]
      * @Return: int
      * @Author: zyc
      * @Date: 2019/8/9 16:43
      */
-    public int countAll(BenchJournalism benchJournalism){
+    public int countAll(BenchJournalism benchJournalism) {
         return benchJournalismMapper.countJournalism(benchJournalism);
     }
 
@@ -64,10 +74,12 @@ public class BenchJournalismServiceImpl  extends BaseServiceImpl<BenchJournalism
     public int create(BenchJournalism journalism) {
         return benchJournalismMapper.save(journalism);
     }
+
     @Override
     public int batchDelete(List<String> ids) {
         return benchJournalismMapper.batchDelete(ids);
     }
+
     @Override
     public int update(BenchJournalism journalism) {
         return benchJournalismMapper.update(journalism);
@@ -75,6 +87,13 @@ public class BenchJournalismServiceImpl  extends BaseServiceImpl<BenchJournalism
 
     @Override
     public BenchJournalism selectById(Long id) {
-        return benchJournalismMapper.selectById(id);
+        BenchJournalism journalism = benchJournalismMapper.selectById(id);
+        if (journalism == null) {
+            return null;
+        }
+
+        String url = ossClientUtil.getUrl(BenchConstant.OSS_FILEDIR, journalism.getPictureOssId());
+        journalism.setPictureOssUrl(url);
+        return journalism;
     }
 }
