c09673355658ee54950e2f6936c5d85183f6f152
zxh <zzhaguo@hotmail.com>
2019-08-23 16:52:11 +0800

    dubbo配置精简：service bean xml配置改为注解

--- log-center/log-center-client/src/main/java/com/shengsu/log/service/LogOpService.java
+++
@@ -1,8 +0,0 @@
-package com.shengsu.log.service;
-
-import com.shengsu.base.service.BaseService;
-import com.shengsu.log.entity.LogOp;
-
-public interface LogOpService extends BaseService<LogOp,String> {
-    public void log(LogOp logOp);
-}
