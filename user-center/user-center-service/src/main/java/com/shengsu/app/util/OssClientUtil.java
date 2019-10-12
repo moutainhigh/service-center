45e92e4bf28818e05ac4498224d3babb47134c7a
zxh <zzhaguo@hotmail.com>
2019-10-12 14:09:39 +0800

    oss工具类修正

--- user-center/user-center-service/src/main/java/com/shengsu/app/util/OssClientUtil.java
+++ user-center/user-center-service/src/main/java/com/shengsu/app/util/OssClientUtil.java
@@ -1,239 +1,67 @@
 package com.shengsu.app.util;
 
 import com.aliyun.oss.OSSClient;
-import com.aliyun.oss.model.ObjectMetadata;
-import com.aliyun.oss.model.PutObjectResult;
-import com.shengsu.app.exception.BizException;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
-import java.io.*;
 import java.net.URL;
 import java.util.Date;
 
 
 /**
  * 阿里云 OSS工具类
- * 
+ *
  * @author Monkey
- * @date 2017年9月30日下午3:38:09
  * @version 1.0
+ * @date 2017年9月30日下午3:38:09
  */
+@Service
 public class OssClientUtil {
- 
-	public static final Logger logger = LoggerFactory.getLogger(OssClientUtil.class);
-	@Value("${oss.endpoint:}")
-	private String endpoint;
-	@Value("${oss.accessKeyId:}")
-	private String accessKeyId;
-	@Value("${oss.accessKeySecret:}")
-	private String accessKeySecret;
-	@Value("${oss.bucketName:}")
-	private String bucketName;
-	// 文件存储目录
-	@Value("${oss.filedir:}")
-	private String filedir;
- 
-	private OSSClient ossClient;
- 
-	/**
-	 * 初始化
-	 */
-	public void init() {
-		ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
-	}
- 
-	/**
-	 * 销毁
-	 */
-	public void destory() {
-		ossClient.shutdown();
-	}
- 
-	/**
-	 * 上传图片
-	 *
-	 * @param url
-	 * @throws BizException
-	 */
-	public void uploadImg2Oss(String url) throws BizException {
-		File fileOnServer = new File(url);
-		FileInputStream fin;
-		try {
-			fin = new FileInputStream(fileOnServer);
-			String[] split = url.split("/");
-			uploadFile2OSS(fin, split[split.length - 1]);
-		} catch (FileNotFoundException e) {
-			throw new BizException("图片上传失败");
-		}
-	}
-	/**
-	 * 获得图片路径
-	 *
-	 * @param fileUrl
-	 * @return
-	 */
-	public String getImgUrl(String fileUrl) {
-		System.out.println(fileUrl);
-		if (!StringUtils.isEmpty(fileUrl)) {
-			String[] split = fileUrl.split("/");
-			return getUrl(this.filedir + split[split.length - 1]);
-		}
-		return null;
-	}
- 
-	/**
-	 * 上传到OSS服务器 如果同名文件会覆盖服务器上的
-	 *
-	 * @param instream
-	 *            文件流
-	 * @param fileName
-	 *            文件名称 包括后缀名
-	 * @return 出错返回"" ,唯一MD5数字签名
-	 */
-	public String uploadFile2OSS(InputStream instream, String fileName) {
-		String ret = "";
-		try {
-			// 创建上传Object的Metadata
-			ObjectMetadata objectMetadata = new ObjectMetadata();
-			objectMetadata.setContentLength(instream.available());
-			objectMetadata.setCacheControl("no-cache");
-			objectMetadata.setHeader("Pragma", "no-cache");
-			objectMetadata.setContentType(getContentType(fileName.substring(fileName.lastIndexOf("."))));
-			objectMetadata.setContentDisposition("inline;filename=" + fileName);
-			// 上传文件
-			init();
-			PutObjectResult putResult = ossClient.putObject(bucketName, filedir + fileName, instream, objectMetadata);
-			ret = putResult.getETag();
-		} catch (IOException e) {
-			logger.error(e.getMessage(), e);
-		} finally {
-			destory();
-			try {
-				if (instream != null) {
-					instream.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return ret;
-	}
-	/**
-	 * 上传到OSS服务器 如果同名文件会覆盖服务器上的
-	 *
-	 * @param instream
-	 *            文件流
-	 * @param fileName
-	 *            文件名称 包括后缀名
-	 * @return 出错返回"" ,唯一MD5数字签名
-	 */
-	public String uploadFile2OSS(InputStream instream, String fileName,String contentType) {
-		String ret = "";
-		try {
-			// 创建上传Object的Metadata
-			ObjectMetadata objectMetadata = new ObjectMetadata();
-			objectMetadata.setContentLength(instream.available());
-			objectMetadata.setCacheControl("no-cache");
-			objectMetadata.setHeader("Pragma", "no-cache");
-			objectMetadata.setContentType(contentType);
-			objectMetadata.setContentDisposition("inline;filename=" + fileName);
-			// 上传文件
-			init();
-			PutObjectResult putResult = ossClient.putObject(bucketName, filedir + fileName, instream, objectMetadata);
-			ret = putResult.getETag();
-		} catch (IOException e) {
-			logger.error(e.getMessage(), e);
-		} finally {
-			destory();
-			try {
-				if (instream != null) {
-					instream.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return ret;
-	}
- 
-	/**
-	 * Description: 判断OSS服务文件上传时文件的contentType
-	 *
-	 * @param filenameExtension 文件后缀
-	 * @return String
-	 */
-	public static String getContentType(String filenameExtension) {
-		if (filenameExtension.equalsIgnoreCase("bmp")) {
-			return "image/bmp";
-		}
-		if (filenameExtension.equalsIgnoreCase("gif")) {
-			return "image/gif";
-		}
-		if (filenameExtension.equalsIgnoreCase("jpeg") || filenameExtension.equalsIgnoreCase("jpg")
-				|| filenameExtension.equalsIgnoreCase("png")) {
-			return "image/jpeg";
-		}
-		if (filenameExtension.equalsIgnoreCase("html")) {
-			return "text/html";
-		}
-		if (filenameExtension.equalsIgnoreCase("txt")) {
-			return "text/plain";
-		}
-		if (filenameExtension.equalsIgnoreCase("js")) {
-			return "application/x-javascript";
-		}
-		if (filenameExtension.equalsIgnoreCase("vsd")) {
-			return "application/vnd.visio";
-		}
-		if (filenameExtension.equalsIgnoreCase("pptx") || filenameExtension.equalsIgnoreCase("ppt")) {
-			return "application/vnd.ms-powerpoint";
-		}
-		if (filenameExtension.equalsIgnoreCase("docx") || filenameExtension.equalsIgnoreCase("doc")) {
-			return "application/msword";
-		}
-		if (filenameExtension.equalsIgnoreCase("xml")) {
-			return "text/xml";
-		}
-		return "image/jpeg";
-	}
- 
-	/**
-	 * 获得url链接
-	 *
-	 * @param key
-	 * @return
-	 */
-	public String getUrl(String key) {
-		// 设置URL过期时间为10年 3600l* 1000*24*365*10
-		Date expiration = new Date(System.currentTimeMillis() + 3600L * 1000 * 24 * 365 * 10);
-		// 生成URL
-		init();
-		URL url = ossClient.generatePresignedUrl(bucketName, filedir+key, expiration);
-		destory();
-		if (url != null) {
-			return url.toString();
-		}
-		return null;
-	}
-	
-	/**
-	 * 删除OSS对象
-	 * @param key
-	 */
-	public void deleteOssFile(String key){
-		init();
-		ossClient.deleteObject(bucketName, filedir+key);
-		destory();
-	}
-	
-	public static String getFileNameNoEx(String filename) {
-		if ((filename != null) && (filename.length() > 0)) {
-			int dot = filename.lastIndexOf('.');
-			filename = filename.substring(dot+1,filename.length());
-		}
-		return filename;
-	}
+
+    public static final Logger logger = LoggerFactory.getLogger(OssClientUtil.class);
+    @Value("${oss.endpoint:}")
+    private String endpoint;
+    @Value("${oss.accessKeyId:}")
+    private String accessKeyId;
+    @Value("${oss.accessKeySecret:}")
+    private String accessKeySecret;
+    @Value("${oss.bucketName:}")
+    private String bucketName;
+
+    private OSSClient ossClient;
+
+    /**
+     * 初始化
+     */
+    public void init() {
+        ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+    }
+    /**
+     * 获得url链接(指定路径）
+     *
+     * @param key
+     * @return
+     */
+    public String getUrl(String filedir, String key) {
+        // 设置URL过期时间为10年 3600l* 1000*24*365*10
+        Date expiration = new Date(System.currentTimeMillis() + 3600L * 1000 * 24 * 365 * 10);
+        // 生成URL
+        init();
+        URL url = ossClient.generatePresignedUrl(bucketName, filedir + key, expiration);
+        destory();
+        if (url != null) {
+            return url.toString();
+        }
+        return null;
+    }
+    /**
+     * 销毁
+     */
+    public void destory() {
+        ossClient.shutdown();
+    }
+
+
 }
