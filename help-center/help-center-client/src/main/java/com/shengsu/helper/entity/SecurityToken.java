42c072598f4ddbd694eb5ca49da803a1a3acd816
zxh <zzhaguo@hotmail.com>
2019-10-12 14:44:46 +0800

    pom文件修正

--- help-center/help-center-client/src/main/java/com/shengsu/helper/entity/SecurityToken.java
+++ help-center/help-center-client/src/main/java/com/shengsu/helper/entity/SecurityToken.java
@@ -1,8 +1,11 @@
 package com.shengsu.helper.entity;
 
+import lombok.Data;
+
 /**
  * 临时token对象
  */
+@Data
 public class SecurityToken {
     private String accessKeyId ;
     private String accessKeySecret;
