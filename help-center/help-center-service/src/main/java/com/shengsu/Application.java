b9bfddc308f0518f114022809ac2e5a870e198c1
zyc <>
2019-11-01 14:09:08 +0800

    去除@ComponentScan注解

--- help-center/help-center-service/src/main/java/com/shengsu/Application.java
+++ help-center/help-center-service/src/main/java/com/shengsu/Application.java
@@ -3,10 +3,10 @@ package com.shengsu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 import java.util.TimeZone;
 
 
@@ -14,19 +14,18 @@ import java.util.TimeZone;
 @EnableAutoConfiguration
 @EnableTransactionManagement
 @EnableAspectJAutoProxy
-@ComponentScan(basePackages = "com.shengsu.util.*")
 @ImportResource({"classpath:application-context.xml"})
 public class Application {
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		try {
-			TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
-			SpringApplication.run(Application.class, args);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+            SpringApplication.run(Application.class, args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
