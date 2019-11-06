31d7237b8d5256355cf638d10c1c4e92150a682b
zyc <>
2019-11-06 10:37:11 +0800

    log错误日志增加推动钉钉消息功能

---
+++ log-center/log-center-client/src/main/java/com/shengsu/log/util/LogErrorUtils.java
@@ -0,0 +1,24 @@
+package com.shengsu.log.util;
+
+import com.shengsu.helper.entity.DingTalkLink;
+import com.shengsu.log.entity.LogError;
+
+/**
+ * Created by zyc on 2019/11/5.
+ */
+public class LogErrorUtils {
+
+    public static DingTalkLink toDingTalkLink(LogError logError, String groupUrl, String msgUrl, String picUrl, String environment) {
+        if (logError != null) {
+            DingTalkLink dingTalkLink = new DingTalkLink();
+            dingTalkLink.setDingGroupUrl(groupUrl);
+            dingTalkLink.setMsgUrl(msgUrl);
+            dingTalkLink.setPicUrl(picUrl);
+            String title = logError.getSystemTag() + "-" + environment + "\n" + logError.getErrName();
+            dingTalkLink.setTitle(title);
+            dingTalkLink.setText(logError.getErrRemark());
+            return dingTalkLink;
+        }
+        return null;
+    }
+}
--- log-center/log-center-service/src/main/java/com/shengsu/log/service/impl/LogErrorServiceImpl.java
+++ log-center/log-center-service/src/main/java/com/shengsu/log/service/impl/LogErrorServiceImpl.java
@@ -2,16 +2,17 @@ package com.shengsu.log.service.impl;
 
 import com.shengsu.base.mapper.BaseMapper;
 import com.shengsu.base.service.impl.BaseServiceImpl;
-import com.shengsu.log.entity.LogBusiness;
+import com.shengsu.helper.entity.DingTalkLink;
+import com.shengsu.helper.service.DingTalkService;
 import com.shengsu.log.entity.LogError;
 import com.shengsu.log.mapper.LogErrorMapper;
 import com.shengsu.log.mq.message.MessageProcessor;
 import com.shengsu.log.service.LogErrorService;
+import com.shengsu.log.util.LogErrorUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author zxh
@@ -21,13 +22,25 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Slf4j
 @Service("logErrorService")
-public class LogErrorServiceImpl extends BaseServiceImpl<LogError, String> implements LogErrorService,MessageProcessor<LogError> {
+public class LogErrorServiceImpl extends BaseServiceImpl<LogError, String> implements LogErrorService, MessageProcessor<LogError> {
 
     /**
      * 错误日志数据访问对象
      */
     @Autowired
     private LogErrorMapper logErrorMapper;
+
+    @Autowired
+    private DingTalkService dingTalkService;
+
+    @Value("${dingtalk.environment}")
+    private String environment;
+    @Value("${dingtalk.groupUrl}")
+    private String groupUrl;
+    @Value("${dingtalk.msgUrl}")
+    private String msgUrl;
+    @Value("${dingtalk.picUrl}")
+    private String picUrl;
 
     @Override
     public BaseMapper<LogError, String> getBaseMapper() {
@@ -41,8 +54,12 @@ public class LogErrorServiceImpl extends BaseServiceImpl<LogError, String> imple
 
     @Override
     public boolean handleMessage(LogError logError) {
-        log.info("处理消息："+logError.toString());
+        log.info("处理消息：" + logError.toString());
+        DingTalkLink dingTalkLink = new DingTalkLink();
         logErrorMapper.save(logError);
+        //发送钉钉消息
+        DingTalkLink dingTalkLink1 = LogErrorUtils.toDingTalkLink(logError, groupUrl, msgUrl, picUrl, environment);
+        dingTalkService.sendLink(dingTalkLink1);
         return true;
     }
 
