3ab685344c8838121aeb895e310aa9c81396116b
zyc <>
2019-11-15 18:29:43 +0800

    修改是否启用案件人员必填参数注解

--- lawcase-center/lawcase-center-client/src/main/java/com/shengsu/system/vo/SystemConfigUpdateDelVo.java
+++ lawcase-center/lawcase-center-client/src/main/java/com/shengsu/system/vo/SystemConfigUpdateDelVo.java
@@ -4,12 +4,13 @@ import lombok.Data;
 import org.hibernate.validator.constraints.NotBlank;
 
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
 /**
  * Created by zyc on 2019/11/15.
  */
 @Data
-public class SystemConfigUpdateDelVo {
+public class SystemConfigUpdateDelVo implements Serializable {
     @NotBlank
     private String userId;
     @NotNull
