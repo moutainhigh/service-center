4d69c12d8c0d7ead56931e1fc736b25235ad088c
zyc <>
2019-11-15 18:17:38 +0800

    增加实体类标记接口

--- lawcase-center/lawcase-center-client/src/main/java/com/shengsu/lawcase/entity/LawcaseJoinUser.java
+++ lawcase-center/lawcase-center-client/src/main/java/com/shengsu/lawcase/entity/LawcaseJoinUser.java
@@ -2,11 +2,13 @@ package com.shengsu.lawcase.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * Created by zyc on 2019/11/15.
  */
 @Data
-public class LawcaseJoinUser {
+public class LawcaseJoinUser implements Serializable{
     private String userId;
     private String userName;
     private String realName;
