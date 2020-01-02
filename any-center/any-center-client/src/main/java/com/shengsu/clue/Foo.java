ebbba07532aa910a8811093edee88f371ee714a9
zxh <zzhaguo@hotmail.com>
2020-01-02 20:05:55 +0800

    案源服务中心

---
+++ any-center/any-center-client/src/main/java/com/shengsu/clue/Foo.java
@@ -0,0 +1,4 @@
+package com.shengsu.clue;
+
+public class Foo {
+}
---
+++ any-center/any-center-service/src/main/java/com/shengsu/Application.java
@@ -0,0 +1,28 @@
+package com.shengsu;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.TimeZone;
+
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableTransactionManagement
+@EnableAspectJAutoProxy
+@ImportResource({"classpath:application-context.xml"})
+public class Application {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+        SpringApplication.run(Application.class, args);
+    }
+
+}
