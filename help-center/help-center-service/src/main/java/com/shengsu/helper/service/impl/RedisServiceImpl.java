7080e5ff0febed8d119f066b6c0f20f4d9118711
zxh <zzhaguo@hotmail.com>
2020-01-17 13:41:33 +0800

    redis服务修正

--- help-center/help-center-service/src/main/java/com/shengsu/helper/service/impl/RedisServiceImpl.java
+++ help-center/help-center-service/src/main/java/com/shengsu/helper/service/impl/RedisServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 @Service("redisService")
 public class RedisServiceImpl implements RedisService {
     @Autowired
-    private static RedisTemplate redisTemplate;
+    private RedisTemplate redisTemplate;
     /**
      * 设置有效时间
      *
