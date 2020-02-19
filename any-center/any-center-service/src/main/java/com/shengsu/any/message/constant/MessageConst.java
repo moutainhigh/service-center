4636e6235f1b6e2db9c56d300a28352db416576c
Bell <2085374271@qq.com>
2020-02-19 16:21:36 +0800

    我的线索工具类改动

--- any-center/any-center-service/src/main/java/com/shengsu/any/message/constant/MessageConst.java
+++ any-center/any-center-service/src/main/java/com/shengsu/any/message/constant/MessageConst.java
@@ -5,4 +5,8 @@ public interface MessageConst {
     String MESSAGE_CONTENT_ACCOUNT_BALANCE_RECHARGE= "您已成功充值{0}元";
     //提现
     String MESSAGE_CONTENT_ACCOUNT_BALANCE_CASH_OUT= "您已成功提现{0}元";
+    //发送用户通过认证消息
+    String MESSAGE_CONTENT_PASS = "您的账号认证已通过";
+    //发送用户未通过认证消息
+    String MESSAGE_CONTENT_REJECT = "您的账号认证未通过";
 }
--- any-center/any-center-service/src/main/java/com/shengsu/any/user/service/impl/UserServiceImpl.java
+++ any-center/any-center-service/src/main/java/com/shengsu/any/user/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.shengsu.any.app.constant.BizConst;
 import com.shengsu.any.app.constant.ResultCode;
 import com.shengsu.any.app.util.RedisUtil;
 import com.shengsu.any.app.util.ResultUtil;
+import com.shengsu.any.message.constant.MessageConst;
 import com.shengsu.any.message.entity.Message;
 import com.shengsu.any.message.service.MessageService;
 import com.shengsu.any.message.util.MessageUtils;
@@ -43,7 +44,7 @@ import static com.shengsu.any.user.util.UserUtils.toUserDetailsPo;
  * @create: 2020-01-07 17:20
  **/
 @Service("anyUserService")
-public class UserServiceImpl extends BaseServiceImpl<User, String> implements UserService,BizConst {
+public class UserServiceImpl extends BaseServiceImpl<User, String> implements UserService,BizConst, MessageConst {
     @Autowired
     private SmsService smsService;
     @Autowired
