b63d3c5832a97570e0322cbe2511f1750a089593
Bell <2085374271@qq.com>
2020-03-19 17:32:59 +0800

    增设咨询服务

---
+++ website-center/website-center-client/src/main/java/com/shengsu/website/consult/entity/LawcaseConsult.java
@@ -0,0 +1,16 @@
+package com.shengsu.website.consult.entity;
+
+import com.shengsu.base.entity.BaseEntity;
+import lombok.Data;
+
+@Data
+public class LawcaseConsult extends BaseEntity {
+    private static final long serialVersionUID = 1L;
+    private String consultId;
+    private String consultContent;
+    private String consultor;
+    private String contact;
+    private String startTime;
+    private String endTime;
+    private String target;
+}
