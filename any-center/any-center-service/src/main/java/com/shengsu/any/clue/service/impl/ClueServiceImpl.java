afd6fdd8f35926f6435b6615db888a059d7e60e9
Bell <2085374271@qq.com>
2020-04-02 15:06:23 +0800

    修改公众号推送信息模板称谓

--- any-center/any-center-service/src/main/java/com/shengsu/any/clue/service/impl/ClueServiceImpl.java
+++ any-center/any-center-service/src/main/java/com/shengsu/any/clue/service/impl/ClueServiceImpl.java
@@ -391,7 +391,7 @@ public class ClueServiceImpl extends BaseServiceImpl<Clue, String> implements Cl
         for (User user : users){
             // 设置模板
             StringBuilder stringBuilder = new StringBuilder(user.getRealName());
-            if(user.getAuthState() != "0"){
+            if(!user.getAuthState().equals(0)){
                 stringBuilder.append("律师");
             }
             TempMessageParamData data = templateMessageService.assembleTemplateDate(TEMPLATE_MESSAGE_CLUE_UPDATE_FIRST_VALUE,stringBuilder.toString(),TEMPLATE_MESSAGE_ACLUE_UPDATE_REMARK_VALUE);
