2fe976b3b81b0abb5a00a18e68e9d6e9b6185bea
lipiao <17602185673@163.com>
2020-04-21 11:36:37 +0800

    消除无用的包导入

--- trade-center/trade-center-service/src/main/java/com/shengsu/trade/mq/service/BdpayNotifyService.java
+++ trade-center/trade-center-service/src/main/java/com/shengsu/trade/mq/service/BdpayNotifyService.java
@@ -1,20 +1,11 @@
 package com.shengsu.trade.mq.service;
 
-import com.alibaba.fastjson.JSON;
 import com.shengsu.mq.MessageProcessor;
-import com.shengsu.trade.pay.entity.PayOrder;
 import com.shengsu.trade.pay.service.BdpayService;
-import com.shengsu.trade.pay.service.PayOrderService;
-import com.shengsu.trade.pay.util.PayOrderUtils;
 import com.shengsu.trade.pay.vo.BdPayNotifyVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
-import java.util.Map;
-
-import static com.shengsu.trade.app.constant.BizConst.*;
 
 /**
  * @description:
