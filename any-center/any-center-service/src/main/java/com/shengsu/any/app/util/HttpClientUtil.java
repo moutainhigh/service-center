2c855c286259991d2ae7e724e2271f2a7f557c9b
zxh <zzhaguo@hotmail.com>
2020-04-29 13:51:33 +0800

    重构优化

--- any-center/any-center-service/src/main/java/com/shengsu/any/app/util/HttpClientUtil.java
+++ any-center/any-center-service/src/main/java/com/shengsu/any/app/util/HttpClientUtil.java
@@ -39,32 +39,6 @@ import java.util.*;
 public class HttpClientUtil {
     private static final Logger logger = LoggerFactory.getLogger(HttpClientUtil.class);
     /**
-     * 从request中获取请求方IP
-     *
-     * @return
-     */
-    public static String getIpAddress() {
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        String ip = request.getHeader("x-forwarded-for");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_CLIENT_IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
-        }
-        return ip;
-    }
-
-    /**
      * @Title: doGet
      * @Description: get方式
      * @author Mundo
