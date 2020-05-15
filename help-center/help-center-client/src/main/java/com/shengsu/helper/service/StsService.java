328dd2cfa4c0e6cceea5e2c409e10c982bf811a3
Bell <2085374271@qq.com>
2020-05-15 10:01:19 +0800

    阿里云登陆接口修剪

--- help-center/help-center-client/src/main/java/com/shengsu/helper/service/StsService.java
+++ help-center/help-center-client/src/main/java/com/shengsu/helper/service/StsService.java
@@ -1,13 +1,8 @@
 package com.shengsu.helper.service;
 
-import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.http.ProtocolType;
-import com.aliyuncs.sts.model.v20150401.AssumeRoleResponse;
 import com.shengsu.helper.entity.SecurityToken;
 
 public interface StsService {
-
-    AssumeRoleResponse assumeRole(String policy,ProtocolType protocolType) throws ClientException;
     SecurityToken getToken();
     SecurityToken getToken(String fileDir);
 }
