db537596888a430761cd19538c47d0453f32669c
Bell <2085374271@qq.com>
2020-05-19 19:44:07 +0800

    校验参数

--- website-center/website-center-client/src/main/java/com/shengsu/website/bdapp/vo/LawyerVo.java
+++ website-center/website-center-client/src/main/java/com/shengsu/website/bdapp/vo/LawyerVo.java
@@ -1,6 +1,7 @@
 package com.shengsu.website.bdapp.vo;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.NotBlank;
 
 import java.io.Serializable;
 
@@ -14,5 +15,6 @@ public class LawyerVo implements Serializable {
     /**
      * 用户id
      */
+    @NotBlank
     private String lawyerId;
 }
