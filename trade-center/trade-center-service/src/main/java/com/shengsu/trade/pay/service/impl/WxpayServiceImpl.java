c602b7ace5d101e7b144a828e487f460f42b8660
lipiao <17602185673@163.com>
2020-05-22 17:43:05 +0800

    微信H5支付-接口服务

--- trade-center/trade-center-service/src/main/java/com/shengsu/trade/pay/service/impl/WxpayServiceImpl.java
+++ trade-center/trade-center-service/src/main/java/com/shengsu/trade/pay/service/impl/WxpayServiceImpl.java
@@ -1,5 +1,7 @@
 package com.shengsu.trade.pay.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.shengsu.helper.service.CodeGeneratorService;
 import com.shengsu.result.ResultBean;
 import com.shengsu.result.ResultUtil;
@@ -10,6 +12,7 @@ import com.shengsu.trade.pay.service.PayOrderService;
 import com.shengsu.trade.pay.service.WxpayService;
 import com.shengsu.trade.pay.util.PayOrderUtils;
 import com.shengsu.trade.pay.vo.WxAppOrderVo;
+import com.shengsu.trade.pay.vo.WxMwebOrderVo;
 import com.shengsu.trade.pay.vo.WxOrderCancelVo;
 import com.shengsu.trade.pay.vo.WxOrderVo;
 import com.shengsu.trade.pay.wxsdk.MyConfig;
@@ -45,6 +48,9 @@ public class WxpayServiceImpl implements WxpayService {
     // 小程序
     @Value("${wxpay.weapp.appid}")
     private String weAppID;
+    // H5
+    @Value("${wxpay.gzh.appid}")
+    private String mwebAppID;
 
     @Autowired
     private CodeGeneratorService codeGeneratorService;
@@ -70,8 +76,8 @@ public class WxpayServiceImpl implements WxpayService {
         MyConfig config= getConfig("WG");
         WXPay wxpay = new WXPay(config, null, true, isSandbox);
         // 添加微信请求公共参数--返回预支付信息
-        Map<String, String> reqData = getOrderRequsetData("案源王充值中心-会员充值",outTradeNo,String.valueOf(totalFee),wxOrderVo.getIpAddress(),notifyUrl,"JSAPI",wxOrderVo.getOpenId());
-        String prepayId;
+        Map<String, String> reqData = getOrderRequsetData("案源王充值中心-会员充值",outTradeNo,String.valueOf(totalFee),wxOrderVo.getIpAddress(),notifyUrl,"JSAPI");
+        reqData.put("openid",wxOrderVo.getOpenId());
         // 构造返回值
         Map<String, String> resp = null;
         Map<String, String> result = null;
@@ -79,7 +85,7 @@ public class WxpayServiceImpl implements WxpayService {
             // 请求微信返回预结果
             resp = wxpay.unifiedOrder(reqData);
             log.info("请求微信返回预期结果"+resp);
-            prepayId = resp.get("prepay_id");
+            String prepayId = resp.get("prepay_id");
             if ("SUCCESS".equals(resp.get("return_code"))&&"SUCCESS".equals(resp.get("result_code"))){
                 // 返回前端数据
                 result =  getOrderResponseData(resp,config,prepayId);
@@ -114,16 +120,16 @@ public class WxpayServiceImpl implements WxpayService {
         MyConfig config= getConfig("WA");
         WXPay wxpay = new WXPay(config, null, true, isSandbox);
         // 添加微信请求公共参数--返回预支付信息
-        Map<String, String> reqData = getOrderRequsetData("小程序支付中心-支付",outTradeNo,String.valueOf(totalFee),wxAppOrderVo.getIpAddress(),notifyUrl,"JSAPI",wxAppOrderVo.getOpenId());
+        Map<String, String> reqData = getOrderRequsetData("小程序支付中心-支付",outTradeNo,String.valueOf(totalFee),wxAppOrderVo.getIpAddress(),notifyUrl,"JSAPI");
+        reqData.put("openid",wxAppOrderVo.getOpenId());
         // 返回数据
-        String prepayId;
         Map<String, String> resp = null;
         Map<String, String> result = null;
         try {
             // 请求微信返回预结果
             resp = wxpay.unifiedOrder(reqData);
             log.info("请求微信返回预期结果"+resp);
-            prepayId = resp.get("prepay_id");
+            String prepayId = resp.get("prepay_id");
             if ("SUCCESS".equals(resp.get("return_code"))&&"SUCCESS".equals(resp.get("result_code"))){
                 // 返回前端数据
                 result =  getOrderResponseData(resp,config,prepayId);
@@ -140,20 +146,79 @@ public class WxpayServiceImpl implements WxpayService {
         }
     }
     /**
+    * @Description: 微信H5下单
+    * @Param: * @Param wxMwebOrderVo: 
+    * @Return: * @return: com.shengsu.result.ResultBean
+    * @date: 
+    */
+    @Override
+    public ResultBean order(WxMwebOrderVo wxMwebOrderVo) throws Exception {
+        log.info("开始下单");
+        int totalFee =  new BigDecimal(wxMwebOrderVo.getAmount()).multiply(new BigDecimal(100)).setScale(2, BigDecimal.ROUND_HALF_UP).intValue();
+        String outTradeNo = codeGeneratorService.generateCode("WMTN");
+        //插入6位随机数
+        outTradeNo=new StringBuilder(outTradeNo).insert(4,PayOrderUtils.randnum(6)).toString();
+        // 配置微信请求参数
+        log.info("配置微信请求参数");
+        MyConfig config= getConfig("WM");
+        WXPay wxpay = new WXPay(config, null, true, isSandbox);
+        // 添加微信请求公共参数--返回预支付信息
+        Map<String, String> reqData = getOrderRequsetData("订单支付-支付",outTradeNo,String.valueOf(totalFee),wxMwebOrderVo.getIpAddress(),notifyUrl,"MWEB");
+        fillSceneInfoData(reqData);
+        // 返回数据
+        Map<String, String> resp = null;
+        Map<String, String> result = new HashMap<>();
+        try {
+            // 请求微信返回预结果
+            resp = wxpay.unifiedOrder(reqData);
+            log.info("请求微信返回预期结果"+resp);
+            String prepayId = resp.get("prepay_id");
+            String mwebUrl =  resp.get("mweb_url");
+            if ("SUCCESS".equals(resp.get("return_code"))&&"SUCCESS".equals(resp.get("result_code"))){
+                // 返回前端数据
+                result.put("mwebUrl", mwebUrl);
+                // order表生成订单数据
+                PayOrder payOrder = PayOrderUtils.toPayOrder("",outTradeNo,prepayId,new BigDecimal(wxMwebOrderVo.getAmount()),PAY_TYPE_WECHAT,ORDER_STATUS_UNPAID);
+                payOrderService.create(payOrder);
+
+            }
+            return ResultUtil.formResult(true, ResultCode.SUCCESS,result);
+
+        } catch (Exception e) {
+            log.error("微信下单请求返回异常"+e);
+            return ResultUtil.formResult(false, ResultCode.FAIL);
+        }
+    }
+    /**
+    * @Description: 填充下单场景信息请求数据
+    * @Param: * @Param reqData: 
+    * @Return: * @return: void
+    * @date: 
+    */
+    private void fillSceneInfoData(Map<String,String> reqData) {
+        JSONObject scene_info = new JSONObject();
+        JSONObject h5_info = new JSONObject();
+        h5_info.put("type","Wap");
+        h5_info.put("wap_url","https://pay.qq.com");
+        h5_info.put("wap_name","订单支付");
+        scene_info.put("h5_info",h5_info);
+        reqData.put("scene_info",JSON.toJSONString(scene_info));
+    }
+
+    /**
     * @Description: 下单请求公共参数(允许添加非必填参数)
     * @Param: * @Param wxOrderVo: 
     * @Return: * @return: java.util.Map<java.lang.String,java.lang.String>
     * @date: 
     */
-    private Map<String,String> getOrderRequsetData(String body,String outTradeNo,String totalFee,String ipAddress,String notifyUrl,String tradeType,String openid) {
+    private Map<String,String> getOrderRequsetData(String body,String outTradeNo,String totalFee,String ipAddress,String notifyUrl,String tradeType) {
         Map<String, String> data = new HashMap<>();
         data.put("body", body);
         data.put("out_trade_no", outTradeNo);
         data.put("total_fee",totalFee);
         data.put("spbill_create_ip", ipAddress);
         data.put("notify_url", notifyUrl);
-        data.put("trade_type", tradeType);// 公众号支付
-        data.put("openid",openid);
+        data.put("trade_type", tradeType);// 交易类型
         return data;
     }
     /**
@@ -223,6 +288,9 @@ public class WxpayServiceImpl implements WxpayService {
                 break;
             case ORDER_FLAG_WECHAT_WEAPP:
                 config.setAppID(weAppID);
+                break;
+            case ORDER_FLAG_WECHAT_MWEB:
+                config.setAppID(mwebAppID);
                 break;
         }
         config.setMchID(mchID);
