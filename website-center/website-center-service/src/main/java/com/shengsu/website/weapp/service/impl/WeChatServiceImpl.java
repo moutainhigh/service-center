28805afcdf670e3e045b73cb8bea4b29adc20862
Bell <2085374271@qq.com>
2020-05-29 17:30:33 +0800

    优化获取节点名称

--- website-center/website-center-service/src/main/java/com/shengsu/website/weapp/service/impl/WeChatServiceImpl.java
+++ website-center/website-center-service/src/main/java/com/shengsu/website/weapp/service/impl/WeChatServiceImpl.java
@@ -13,7 +13,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import static com.shengsu.website.app.constant.BizConst.SYSTEM_TAG_SHENGSU;
+
 /**
  * @program: service-center
  * @author: Bell
@@ -36,21 +38,21 @@ public class WeChatServiceImpl implements WeChatService {
     private String ysAppsecret;
 
     @Override
-    public ResultBean authorize(WeChatVo weChatVo){
-        if(SYSTEM_TAG_SHENGSU.equals(weChatVo.getSystemTag())){
-        HashMap<String, String> resultMap = new HashMap<>();
-        Map<String, String> params = new HashMap<String, String>();
-        params.put("appid", ssAppID);
-        params.put("secret", ssAppsecret);
-        params.put("js_code",weChatVo.getCode());
-        params.put("grant_type","authorization_code");
-        String httpResultStr = HttpClientUtil.doGet(ssAccessLoginUrl, params);
-        JSONObject jSONObject = JSON.parseObject(httpResultStr);
-        String openid = String.valueOf(jSONObject.get("openid"));
-        String sessionKey = String.valueOf(jSONObject.get("session_key"));
-        resultMap.put("openid", openid);
-        resultMap.put("session_key", sessionKey);
-        return ResultUtil.formResult(true, ResultCode.SUCCESS,resultMap);
+    public ResultBean authorize(WeChatVo weChatVo) {
+        if (SYSTEM_TAG_SHENGSU.equals(weChatVo.getSystemTag())) {
+            HashMap<String, String> resultMap = new HashMap<>();
+            Map<String, String> params = new HashMap<String, String>();
+            params.put("appid", ssAppID);
+            params.put("secret", ssAppsecret);
+            params.put("js_code", weChatVo.getCode());
+            params.put("grant_type", "authorization_code");
+            String httpResultStr = HttpClientUtil.doGet(ssAccessLoginUrl, params);
+            JSONObject jSONObject = JSON.parseObject(httpResultStr);
+            String openid = String.valueOf(jSONObject.get("openid"));
+            String sessionKey = String.valueOf(jSONObject.get("session_key"));
+            resultMap.put("openid", openid);
+            resultMap.put("session_key", sessionKey);
+            return ResultUtil.formResult(true, ResultCode.SUCCESS, resultMap);
         } else {
             HashMap<String, String> resultMap = new HashMap<>();
             Map<String, String> params = new HashMap<String, String>();
