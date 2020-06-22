af3f3991599bc49055f2f57fa452bf8a44403de5
Bell <2085374271@qq.com>
2020-06-22 15:52:07 +0800

    小程序增加原价字段

--- website-center/website-center-client/src/main/java/com/shengsu/website/market/entity/SystemConfig.java
+++ website-center/website-center-client/src/main/java/com/shengsu/website/market/entity/SystemConfig.java
@@ -8,5 +8,7 @@ import java.math.BigDecimal;
 @Data
 public class SystemConfig extends BaseEntity {
     private BigDecimal onlineConsultFee;
+    private BigDecimal onlineConsultFeeOld;
     private BigDecimal telConsultFee;
+    private BigDecimal telConsultFeeOld;
 }
