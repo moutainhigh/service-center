add4a9757921352249e347632e0d3b0f6026e238
zxh <zzhaguo@hotmail.com>
2020-06-28 11:42:37 +0800

    代码重构

--- website-center/website-center-client/src/main/java/com/shengsu/website/market/util/KeyWordUtils.java
+++ website-center/website-center-client/src/main/java/com/shengsu/website/market/util/KeyWordUtils.java
@@ -5,6 +5,7 @@ import com.shengsu.website.market.po.LawKnowledgeDetailsPo;
 import com.shengsu.website.market.po.LawKnowledgeQueryPo;
 import org.apache.commons.lang3.StringUtils;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,80 +16,70 @@ import java.util.List;
  * @create: 2020-06-23 16:24
  **/
 public class KeyWordUtils {
+    static String head = "<span style=\"color: rgb(249, 150, 59);\">（{0}）</span> <p>";
+    static String tail = "<span style=\"color: rgb(249, 150, 59);\">（{0}）</span> <p>";
+    static String body = "<span style=\"color: rgb(249, 150, 59);\">（{0}）</span>";
+    static List<String> keywords = Arrays.asList(KeyWord.listKeyWord.split("，"));
+
     public static void addKeyWord(LawKnowledgeQueryPo lawKnowledgeQueryPo ,String city){
-        List<String> words = Arrays.asList(KeyWord.listKeyWord.split("，"));
-        String result = lawKnowledgeQueryPo.getContent();
-        String reg = "<p>[\\s]*</p>";
-        String firstContent = result.replaceAll(reg,"");
-        String regex = "<p><br></p>";
-        String content = firstContent.replaceAll(regex,"");
-        List<Integer> list = new ArrayList<>();
-        //定位段落
-        for (int index = 0; index < content.length(); index++) {
-            index = content.indexOf("</p>", index);//获取</p>下标，从而找到插入位置
-            if (index < 0) {
-                break;
-            }
-            list.add(index);
-        }
-
-        StringBuilder sb = new StringBuilder(content);//将String变成StringBuilder，字符串可编辑模式
-        for (int i = list.size() - 1; i >= 0; i--) {
-            if (Math.random() > 0.3) {
-                sb.insert(list.get(i), "<span style=\"color: rgb(249, 150, 59);\">" + "（" + words.get((int) (Math.random() * words.size())) + "）</span>");
-            }
-        }//随机插入
-        if (StringUtils.isNotBlank(lawKnowledgeQueryPo.getThirdCategoryName())) {
-            sb.insert(0, "<span style=\"color: rgb(249, 150, 59);\">" + "（" +city +"  "+ lawKnowledgeQueryPo.getFirstCategoryName() + "  " + lawKnowledgeQueryPo.getSecondCategoryName() + "  " + lawKnowledgeQueryPo.getThirdCategoryName() + words.get((int) (Math.random() * words.size())) + ") </span> <p>");
-            sb.append("<span style=\"color: rgb(249, 150, 59);\">" + "（" +city +"  "+ lawKnowledgeQueryPo.getFirstCategoryName() + "  " + lawKnowledgeQueryPo.getSecondCategoryName() + "  " + lawKnowledgeQueryPo.getThirdCategoryName() + words.get((int) (Math.random() * words.size())) + ") </span> <p>");
-        }
-
-        if (StringUtils.isNotBlank(lawKnowledgeQueryPo.getSecondCategoryName()) && StringUtils.isBlank(lawKnowledgeQueryPo.getThirdCategoryName())) {
-            sb.insert(0, "<span style=\"color: rgb(249, 150, 59);\">" + "（" +city +"  "+ lawKnowledgeQueryPo.getFirstCategoryName() + "  " + lawKnowledgeQueryPo.getSecondCategoryName() + "  " + words.get((int) (Math.random() * words.size())) + ") </span> <p>");
-            sb.append("<span style=\"color: rgb(249, 150, 59);\">" + "（" +city+"  "+ lawKnowledgeQueryPo.getFirstCategoryName() + "  " + lawKnowledgeQueryPo.getSecondCategoryName() + "  " + words.get((int) (Math.random() * words.size())) + ") </span> <p>");
-        }
-        if (StringUtils.isBlank(lawKnowledgeQueryPo.getThirdCategoryName()) && StringUtils.isBlank(lawKnowledgeQueryPo.getSecondCategoryName())) {
-            sb.insert(0, "<span style=\"color: rgb(249, 150, 59);\">" + "（" +city +"  "+ lawKnowledgeQueryPo.getFirstCategoryName() + "  " + words.get((int) (Math.random() * words.size())) + ") </span> <p>");
-            sb.append("<span style=\"color: rgb(249, 150, 59);\">" + "（" +city+"  "+ lawKnowledgeQueryPo.getFirstCategoryName() + "  " + words.get((int) (Math.random() * words.size())) + ") </span> <p>");
-        }
-
-        lawKnowledgeQueryPo.setContent(sb.toString());
+        String content = lawKnowledgeQueryPo.getContent();
+        String firstCategoryName = lawKnowledgeQueryPo.getFirstCategoryName();
+        String secondCategoryName = lawKnowledgeQueryPo.getSecondCategoryName();
+        String thirdCategoryName = lawKnowledgeQueryPo.getThirdCategoryName();
+        content = formatContent(content,city,firstCategoryName,secondCategoryName,thirdCategoryName);
+        lawKnowledgeQueryPo.setContent(content);
     }
     public static void addKeyWord(LawKnowledgeDetailsPo lawKnowledgeDetailsPo,String city){
-        List<String> words = Arrays.asList(KeyWord.listKeyWord.split("，"));
-        String result = lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getContent();
+        String content = lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getContent();
+        String firstCategoryName = lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getFirstCategoryName();
+        String secondCategoryName = lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getSecondCategoryName();
+        String thirdCategoryName = lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getThirdCategoryName();
+        content = formatContent(content,city,firstCategoryName,secondCategoryName,thirdCategoryName);
+        lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().setContent(content);
+    }
+
+
+    /**
+     * 格式化内容
+     * @param content
+     * @param city
+     * @param firstCategoryName
+     * @param sencondCategoryName
+     * @param thirdCatetoryName
+     * @return
+     */
+    private static String formatContent(String content, String city, String firstCategoryName, String sencondCategoryName, String thirdCatetoryName){
         String reg = "<p>[\\s]*</p>";
-        String firstContent = result.replaceAll(reg,"");
+        content = content.replaceAll(reg,"");
         String regex = "<p><br></p>";
-        String content = firstContent.replaceAll(regex,"");
+        content = content.replaceAll(regex,"");
         List<Integer> list = new ArrayList<>();
         //定位段落
         for (int index = 0; index < content.length(); index++) {
-            index = content.indexOf("</p>", index);//获取</p>下标，从而找到插入位置
+            //获取</p>下标，从而找到插入位置
+            index = content.indexOf("</p>", index);
             if (index < 0) {
                 break;
             }
             list.add(index);
         }
-
-        StringBuilder sb = new StringBuilder(content);//将String变成StringBuilder，字符串可编辑模式
+        //将String变成StringBuilder，字符串可编辑模式
+        StringBuilder sb = new StringBuilder(content);
+        String randomWord = keywords.get((int) (Math.random() * keywords.size()));
         for (int i = list.size() - 1; i >= 0; i--) {
             if (Math.random() > 0.3) {
-                sb.insert(list.get(i), "<span style=\"color: rgb(249, 150, 59);\">" + "（" + words.get((int) (Math.random() * words.size())) + "）</span>");
+                sb.insert(list.get(i), MessageFormat.format(body,randomWord));
             }
-        }//随机插入
-        if (StringUtils.isNotBlank(lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getThirdCategoryName())) {
-            sb.insert(0, "<span style=\"color: rgb(249, 150, 59);\">" + "（" +city +"  "+ lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getFirstCategoryName() + "  " + lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getSecondCategoryName() + "  " + lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getThirdCategoryName() + words.get((int) (Math.random() * words.size())) + ") </font> <p>");
-            sb.append("<span style=\"color: rgb(249, 150, 59);\">" + "（" +city +"  "+ lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getFirstCategoryName() + "  " + lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getSecondCategoryName() + "  " + lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getThirdCategoryName() + words.get((int) (Math.random() * words.size())) + ") </span> <p>");
         }
-        if (StringUtils.isNotBlank(lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getSecondCategoryName()) && StringUtils.isBlank(lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getThirdCategoryName())) {
-            sb.insert(0, "<span style=\"color: rgb(249, 150, 59);\">" + "（" +city +"  "+ lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getFirstCategoryName() + "  " + lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getSecondCategoryName() + "  " + words.get((int) (Math.random() * words.size())) + ") </span> <p>");
-            sb.append("<span style=\"color: rgb(249, 150, 59);\">" + "（" +city +"  "+ lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getFirstCategoryName() + "  " + lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getSecondCategoryName() + "  " + words.get((int) (Math.random() * words.size())) + ") </span> <p>");
-        }
-        if(StringUtils.isBlank(lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getThirdCategoryName()) && StringUtils.isBlank(lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getSecondCategoryName())){
-        sb.insert(0, "<span style=\"color: rgb(249, 150, 59);\">" + "（" +city +"  " + lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getFirstCategoryName() + "  " + words.get((int) (Math.random() * words.size())) + ") </span> <p>");
-        sb.append("<span style=\"color: rgb(249, 150, 59);\">" + "（" +city +"  "+ lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().getFirstCategoryName() + "  " + words.get((int) (Math.random() * words.size())) + ") </span> <p>");}
-        lawKnowledgeDetailsPo.getLawKnowledgeCurrentPo().setContent(sb.toString());
+        //随机插入
+        firstCategoryName = StringUtils.isBlank(firstCategoryName)?"":" "+firstCategoryName;
+        sencondCategoryName = StringUtils.isBlank(sencondCategoryName)?"":" "+sencondCategoryName;
+        thirdCatetoryName = StringUtils.isBlank(thirdCatetoryName)?"":" "+thirdCatetoryName;
+        String headTail = city+firstCategoryName+ sencondCategoryName+thirdCatetoryName;
+        randomWord = keywords.get((int) (Math.random() * keywords.size()));
+        sb.insert(0, MessageFormat.format(headTail,headTail+randomWord));
+        randomWord = keywords.get((int) (Math.random() * keywords.size()));
+        sb.append(MessageFormat.format(headTail,headTail+randomWord));
+        return sb.toString();
     }
-
 }
