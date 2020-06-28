bd4e032c8b9873a6b771f0f930f0074e489715f1
Bell <2085374271@qq.com>
2020-06-28 20:45:49 +0800

    按需求添加关键字

--- website-center/website-center-service/src/main/java/com/shengsu/website/market/util/KeyWordUtils.java
+++ website-center/website-center-service/src/main/java/com/shengsu/website/market/util/KeyWordUtils.java
@@ -41,6 +41,7 @@ public class KeyWordUtils {
 
     /**
      * 格式化内容
+     *
      * @param content
      * @param city
      * @param firstCategoryName
@@ -48,39 +49,100 @@ public class KeyWordUtils {
      * @param thirdCatetoryName
      * @return
      */
-    private static String formatContent(String content, String city, String firstCategoryName, String sencondCategoryName, String thirdCatetoryName){
-        String reg = "<p>[\\s]*</p>";
-        content = content.replaceAll(reg,"");
-        String regex = "<p><br></p>";
-        content = content.replaceAll(regex,"");
+    private static String formatContent(String content, String city, String firstCategoryName, String sencondCategoryName, String thirdCatetoryName) {
+        content = content.replaceAll("\\&[a-zA-Z]{1,10};", "").replaceAll("<[^>]*>", "").replaceAll("[(/>)<]", "");
         List<Integer> list = new ArrayList<>();
-        //定位段落
-        for (int index = 0; index < content.length(); index++) {
-            //获取</p>下标，从而找到插入位置
-            index = content.indexOf("</p>", index);
-            if (index < 0) {
-                break;
-            }
-            list.add(index);
+        if (content.length() <= 500) {
+            int finalPosition = getMinPosition(content, 0.5);
+            list.add(finalPosition);
         }
+        if (content.length() <= 1500 && content.length() > 500) {
+            int finalPosition = getMinPosition(content, 0.4);
+            list.add(finalPosition);
+            finalPosition = getMinPosition(content, 0.8);
+            list.add(finalPosition);
+        }
+        if (content.length() <= 3000 && content.length() > 1500) {
+            int finalPosition = getMinPosition(content, 0.3);
+            list.add(finalPosition);
+            finalPosition = getMinPosition(content, 0.6);
+            list.add(finalPosition);
+            finalPosition = getMinPosition(content, 0.9);
+            list.add(finalPosition);
+        }
+        if (content.length() <= 6000 && content.length() > 3000) {
+            int finalPosition = getMinPosition(content, 0.25);
+            list.add(finalPosition);
+            finalPosition = getMinPosition(content, 0.5);
+            list.add(finalPosition);
+            finalPosition = getMinPosition(content, 0.75);
+            list.add(finalPosition);
+            list.add(content.length());
+        }
+        if (content.length() > 6000) {
+            int finalPosition = getMinPosition(content, 0.2);
+            list.add(finalPosition);
+            finalPosition = getMinPosition(content, 0.4);
+            list.add(finalPosition);
+            finalPosition = getMinPosition(content, 0.6);
+            list.add(finalPosition);
+            finalPosition = getMinPosition(content, 0.8);
+            list.add(finalPosition);
+            list.add(content.length());
+        }
+
         //将String变成StringBuilder，字符串可编辑模式
         StringBuilder sb = new StringBuilder(content);
         String randomWord;
         for (int i = list.size() - 1; i >= 0; i--) {
             randomWord = keywords.get((int) (Math.random() * keywords.size()));
-            if (Math.random() > 0.3) {
-                sb.insert(list.get(i), MessageFormat.format(body,randomWord));
-            }
+            sb.insert(list.get(i), MessageFormat.format(body, randomWord));
         }
+
+
         //随机插入
-        firstCategoryName = StringUtils.isBlank(firstCategoryName)?"":" "+firstCategoryName;
-        sencondCategoryName = StringUtils.isBlank(sencondCategoryName)?"":" "+sencondCategoryName;
-        thirdCatetoryName = StringUtils.isBlank(thirdCatetoryName)?"":" "+thirdCatetoryName;
-        String headTail = city+firstCategoryName+ sencondCategoryName+thirdCatetoryName;
-        randomWord =" "+ keywords.get((int) (Math.random() * keywords.size()));
-        sb.insert(0, MessageFormat.format(head,headTail+randomWord));
-        randomWord =" "+ keywords.get((int) (Math.random() * keywords.size()));
-        sb.append(MessageFormat.format(tail,headTail+randomWord));
+        firstCategoryName = StringUtils.isBlank(firstCategoryName) ? "" : " " + firstCategoryName;
+        sencondCategoryName = StringUtils.isBlank(sencondCategoryName) ? "" : " " + sencondCategoryName;
+        thirdCatetoryName = StringUtils.isBlank(thirdCatetoryName) ? "" : " " + thirdCatetoryName;
+        String headTail = city + firstCategoryName + sencondCategoryName + thirdCatetoryName;
+        randomWord = " " + keywords.get((int) (Math.random() * keywords.size()));
+        sb.insert(0, MessageFormat.format(head, headTail + randomWord));
+        randomWord = " " + keywords.get((int) (Math.random() * keywords.size()));
+        sb.append(MessageFormat.format(tail, headTail + randomWord));
         return sb.toString();
+    }
+
+    /**
+     * @return int
+     * @Author Bell
+     * @Description 据最佳位置最近点
+     * @Date 2020/6/28
+     * @Param [content, proportion]
+     **/
+    public static int getMinPosition(String content, double proportion) {
+        int total = content.length();
+        int position = (int) Math.round(total * proportion);
+        int backward = content.indexOf("。", position);
+        String previou = content.substring(0, position + 1);
+        previou = reverseByRecursion(previou);
+        int forward = previou.length() - previou.indexOf("。");
+        int finalPosition = backward - forward < 0 ? backward : forward;
+        return finalPosition;
+    }
+
+    /**
+     * @return java.lang.String
+     * @Author Bell
+     * @Description 反转字符串
+     * @Date 2020/6/28
+     * @Param [str]
+     **/
+    public static String reverseByRecursion(String str) {
+        int length = str.length();
+        if (length <= 1) {
+            return str;
+        }
+
+        return reverseByRecursion(str.substring(1)) + str.charAt(0);
     }
 }
