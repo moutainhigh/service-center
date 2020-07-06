318561588ce2ceadad506483378abed20dcecc44
Bell <2085374271@qq.com>
2020-07-06 10:00:27 +0800

    文章判空处理

--- website-center/website-center-service/src/main/java/com/shengsu/website/market/service/impl/LawKnowledgeServiceImpl.java
+++ website-center/website-center-service/src/main/java/com/shengsu/website/market/service/impl/LawKnowledgeServiceImpl.java
@@ -113,6 +113,9 @@ public class LawKnowledgeServiceImpl extends BaseServiceImpl<LawKnowledge, Strin
     @Override
     public ResultBean<LawKnowledgeQueryPo> queryWithKeyWord(LawKnowledgeQueryVo lawKnowledgeQueryVo) {
         LawKnowledgeQueryPo lawKnowledgeQueryPo = (LawKnowledgeQueryPo) query(lawKnowledgeQueryVo).getBody();
+        if (lawKnowledgeQueryPo == null) {
+            return ResultUtil.formResult(false, ResultCode.LAW_KNOWLEDGE_ID_ERROR, null);
+        }
         KeyWordUtils.addKeyWord(lawKnowledgeQueryPo,lawKnowledgeQueryVo.getCity());
         return ResultUtil.formResult(true,ResultCode.SUCCESS,lawKnowledgeQueryPo);
     }
