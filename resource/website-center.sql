43028714057bbd1698df6730a0de0b483b71d9c7
Bell <2085374271@qq.com>
2020-07-08 17:41:48 +0800

    律师表结构变更

--- resource/website-center.sql
+++ resource/website-center.sql
@@ -55,7 +55,7 @@ CREATE TABLE `lawcase_consult` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 --律师表结构变更
 alter table lawyer
-add column introduction varchar(10) default '' comment '简介',
+add column introduction varchar(500) default '' comment '简介',
 add column overall_evaluation varchar(10) default '' comment '综合评估',
 add column professional_degree varchar(10) default '' comment '专业程度',
 add column service_efficiency varchar(10) default '' comment '服务效率',
