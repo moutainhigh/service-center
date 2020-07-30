964e769005b02466cb4a378662099906eea313b5
Bell <2085374271@qq.com>
2020-07-30 18:01:20 +0800

    不展示system_tag

--- website-center/website-center-service/src/main/java/com/shengsu/website/market/util/LawyerUtils.java
+++ website-center/website-center-service/src/main/java/com/shengsu/website/market/util/LawyerUtils.java
@@ -77,7 +77,6 @@ public class LawyerUtils {
         lawyerPo.setProfessionalDegree(lawyer.getProfessionalDegree());
         lawyerPo.setServiceAttitude(lawyer.getServiceAttitude());
         lawyerPo.setServiceEfficiency(lawyer.getServiceEfficiency());
-        lawyerPo.setSystemTag(lawyer.getSystemTag());
         return lawyerPo;
     }
 }
